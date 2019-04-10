ccb74664368c4f3af082dbf0677ec1c601905546
huangsc <huangsc@hst.com>
2019-04-10 18:29:19 +0800

    更新settings文件

--- ezTalksProject/settings.py
+++ ezTalksProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u2o5!_w7-zczimf-c=q=4-&8y3%4072mh+r)e8uiap#q!twb)c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -131,6 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 
